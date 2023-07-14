96f1c86bb923ccea404f07b440b9ccc1c1b245c6
Jason Zubiate <jason@Jasons-Mac-mini.local>
2023-07-13 20:14:19 -0700

    router for next auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,12 +11,15 @@ function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient"></div>
-        </div>
-        <main className="app">
-          <Nav/>
-          {children}</main>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
