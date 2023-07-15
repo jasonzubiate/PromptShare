65e02dacc5c845c79b07cca50a6d739f38308cf7
Jason Zubiate <jason@Jasons-Mac-mini.local>
2023-07-15 15:55:57 -0700

    padding

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <section className="flex justify-center items-center w-full mt-8">
+    <section className="flex justify-center items-center w-full my-8">
       <p className="text-stone-800">Built by <a className="hover:text-stone-500" href="https://www.jasonzubiate.com/">Jason Zubiate</a></p>
     </section>
   )
